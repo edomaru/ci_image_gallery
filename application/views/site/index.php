904424dfe21397f74692482cc0763981882aec13
edo masaru <masaruedo@yahoo.com>
2015-10-04 10:36:03 +0700

    load images data from database and show in site index

--- application/views/site/index.php
+++ application/views/site/index.php
@@ -2,77 +2,22 @@
 
 <div class="row">
 
-  <div class="col-lg-3 col-md-4 col-sm-6">
-    <div class="thumbnail">
-      <a href="detail.html"><img src="<?php echo base_url(); ?>assets/uploads/Image+1.jpg" alt="Image 1"></a>
-      <div class="thumbnail-caption">
-        <h4><a href="detail.html">Image 1</a></h4>
-      </div>
-    </div>
-  </div><!-- /.col-lg-3.col-md-4.col-sm-6 -->
+  <?php if ($query->num_rows()): foreach ($query->result() as $row): ?>
 
-  <div class="col-lg-3 col-md-4 col-sm-6">
-    <div class="thumbnail">
-      <a href="detail.html"><img src="<?php echo base_url(); ?>assets/uploads/Image+2.jpg" alt="Image 2"></a>
-      <div class="thumbnail-caption">
-        <h4><a href="detail.html">Image 2</a></h4>
-      </div>
-    </div>
-  </div><!-- /.col-lg-3.col-md-4.col-sm-6 -->
+    <div class="col-lg-3 col-md-4 col-sm-6">
+      <div class="thumbnail">
+        <?php
+          $thumbnail = empty($row->thumbnail) ? $row->filename : $row->thumbnail;
+        ?>
 
-  <div class="col-lg-3 col-md-4 col-sm-6">
-    <div class="thumbnail">
-      <a href="detail.html"><img src="<?php echo base_url(); ?>assets/uploads/Image+3.jpg" alt="Image 3"></a>
-      <div class="thumbnail-caption">
-        <h4><a href="detail.html">Image 3</a></h4>
+        <a href="<?php echo site_url("site/detail/" . $row->id) ?>"><img src="<?php echo base_url(); ?>assets/uploads/<?php echo $thumbnail; ?>" alt="<?php echo $row->title ?>"></a>
+        <div class="thumbnail-caption">
+          <h4><a href="<?php echo site_url("site/detail/" . $row->id) ?>"><?php echo $row->title; ?></a></h4>
+        </div>
       </div>
-    </div>
-  </div><!-- /.col-lg-3.col-md-4.col-sm-6 -->
+    </div><!-- /.col-lg-3.col-md-4.col-sm-6 -->
 
-  <div class="col-lg-3 col-md-4 col-sm-6">
-    <div class="thumbnail">
-      <a href="detail.html"><img src="<?php echo base_url(); ?>assets/uploads/Image+4.jpg" alt="Image 4"></a>
-      <div class="thumbnail-caption">
-        <h4><a href="detail.html">Image 4</a></h4>
-      </div>
-    </div>
-  </div><!-- /.col-lg-3.col-md-4.col-sm-6 -->
-
-  <div class="col-lg-3 col-md-4 col-sm-6">
-    <div class="thumbnail">
-      <a href="detail.html"><img src="<?php echo base_url(); ?>assets/uploads/Image+5.jpg" alt="Image 5"></a>
-      <div class="thumbnail-caption">
-        <h4><a href="detail.html">Image 5</a></h4>
-      </div>
-    </div>
-  </div><!-- /.col-lg-3.col-md-4.col-sm-6 -->
-
-  <div class="col-lg-3 col-md-4 col-sm-6">
-    <div class="thumbnail">
-      <a href="detail.html"><img src="<?php echo base_url(); ?>assets/uploads/Image+6.jpg" alt="Image 6"></a>
-      <div class="thumbnail-caption">
-        <h4><a href="detail.html">Image 6</a></h4>
-      </div>
-    </div>
-  </div><!-- /.col-lg-3.col-md-4.col-sm-6 -->
-
-  <div class="col-lg-3 col-md-4 col-sm-6">
-    <div class="thumbnail">
-      <a href="detail.html"><img src="<?php echo base_url(); ?>assets/uploads/Image+7.jpg" alt="Image 7"></a>
-      <div class="thumbnail-caption">
-        <h4><a href="detail.html">Image 7</a></h4>
-      </div>
-    </div>
-  </div><!-- /.col-lg-3.col-md-4.col-sm-6 -->
-
-  <div class="col-lg-3 col-md-4 col-sm-6">
-    <div class="thumbnail">
-      <a href="detail.html"><img src="<?php echo base_url(); ?>assets/uploads/Image+8.jpg" alt="Image 8"></a>
-      <div class="thumbnail-caption">
-        <h4><a href="detail.html">Image 8</a></h4>
-      </div>
-    </div>
-  </div><!-- /.col-lg-3.col-md-4.col-sm-6 -->
+  <?php endforeach; endif; ?>
 
 </div><!-- ./row -->
 
