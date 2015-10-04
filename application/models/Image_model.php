aaafa5b495bcacf7d1babb537d988fe8813cc1af
edo masaru <masaruedo@yahoo.com>
2015-10-05 06:14:25 +0700

    - tutorial for public page done

---
+++ application/models/Image_model.php
@@ -0,0 +1,59 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Image_model extends CI_Model {
+
+	private $table = "images";
+
+	public function all($limit = 8)
+	{
+		$this->db->limit($limit);
+		$this->db->offset( $this->uri->segment(3) );
+		return $this->db->get($this->table);
+	}
+
+	public function count()
+	{
+		return $this->db->count_all_results($this->table);
+	}
+
+	public function latest()
+	{
+		$this->db->order_by("created_at", "desc");
+
+		return $this;
+	}
+
+	public function popular()
+	{
+		$this->db->order_by("download_count", "desc");
+
+		return $this;
+	}
+
+	public function find($id)
+	{
+		$this->db->where("id", $id);
+		$query = $this->db->get($this->table);
+		return $query->num_rows() ? $query->row() : null;
+	}
+
+	public function random_images($except, $limit = 2)
+	{
+		return $this->db->query("SELECT * FROM {$this->table} WHERE id != ? ORDER BY RAND() LIMIT ?", array($except, $limit));
+	}
+
+	public function view_count($id)
+	{
+		$this->db->set("view_count", "view_count+1", false);
+		$this->db->where("id", $id);
+		$this->db->update($this->table);
+	}
+
+	public function download_count($id)
+	{
+		$this->db->set("download_count", "download_count+1", false);
+		$this->db->where("id", $id);
+		$this->db->update($this->table);
+	}
+
+}
---
+++ application/views/footer.php
@@ -0,0 +1,12 @@
+	</div><!-- /.container -->
+	
+    <footer> 
+        <p class="text-center">&copy; 2015 Cimage</p>                  
+    </footer>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
